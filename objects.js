878507a153fcb888587ca81ad05230b209f16852
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-08-19 15:32:06 +0500

    add examples for square brackets, dynamic property names, and property value shorthand

--- objects.js
+++ objects.js
@@ -52,3 +52,55 @@ const user2 = new User("Bob", 30, "New York");
 
 console.log(user1);
 console.log(user2);
+
+/**
+ * Square brackets
+ * Square brackets are used to access object properties. These are much more powerful than dot notation, especially when dealing with dynamic property names.
+ * Example:
+ */
+
+const user = {};
+
+// set
+user["likes birds"] = true;
+
+// get
+console.log(user["likes birds"]);
+
+user["age"] = 26;
+let key = "age";
+console.log(user.key); // This will log 'key' as undefined
+console.log(user[key]);
+
+// delete
+delete user["likes birds"];
+
+console.log(user);
+
+/**
+ * Dynamic Property Names
+ */
+
+let fruit = "apple";
+let bag = {
+  [fruit + "Computers"]: 5, // bag.appleComputers = 5
+};
+
+console.log(bag);
+
+/**
+ * Property value shorthand
+ * If key and property name are the same, you can omit the key
+ */
+
+let custume = "jocker";
+let age = 30;
+
+const userNew = {
+  custume,
+  age,
+};
+
+console.log(userNew);
+
+
